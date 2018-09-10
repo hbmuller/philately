e2100a04488c8ba095e2fe6221beb0da2efb63a7
Henrique <hbmuller@gmail.com>
2018-09-10 18:08:00 +0200

    Finish RushLayer

--- src/RushLayer.js
+++ src/RushLayer.js
@@ -1,11 +1,11 @@
-import { isUndefined, isNumber } from "lodash";
+import { isUndefined, isNumber } from 'lodash';
 import {
   DEFAULT_LAYER_CONFIG,
   VALID_SOURCE_TYPES,
   ERROR_INVALID_SOURCE,
-  ERROR_INVALID_OPACITY
-} from "./constants";
-import { getSourceElement } from "./utils";
+  ERROR_INVALID_OPACITY,
+} from './constants';
+import { getSourceElement } from './utils';
 
 class RushLayer {
   constructor(config) {
@@ -14,7 +14,7 @@ class RushLayer {
     this.setSource(source);
 
     if (this.source) {
-      this.isActive = !!isActive;
+      this.setActive(isActive);
       this.setPosition({ x, y });
       this.setOpacity(opacity);
     }
@@ -24,35 +24,32 @@ class RushLayer {
     const sourceElement = getSourceElement(source);
 
     if (!sourceElement) {
-      this.isActive = false;
+      this.setActive(false);
       return console.error(ERROR_INVALID_SOURCE);
     }
 
     return (this.source = sourceElement);
   }
 
-  toggle = () => (this.isActive = !this.isActive);
+  getActive = () => this.isActive;
+
+  setActive = (isActive = true) => (this.isActive = !!isActive);
+
+  toggleActive = () => (this.isActive = !this.isActive);
 
   getPosition = () => this.position;
 
-  setPosition = ({ x, y }) =>
-    (this.position = {
-      x: isNumber(x) ? x : this.position.x,
-      y: isNumber(y) ? x : this.position.y
-    });
+  setPosition = ({ x, y }) => {
+    if (isNumber(x)) this.position.x = x;
+    if (isNumber(y)) this.position.y = y;
+
+    return this.position;
+  };
 
   getOpacity = () => this.opacity;
 
-  setOpacity(opacity) {
-    if (!isNumber(opacity)) {
-      console.error(ERROR_INVALID_OPACITY);
-      return this.opacity;
-    }
-
-    this.opacity = Math.max(0, Math.min(1, opacity));
-
-    return this.opacity;
-  }
+  setOpacity = opacity =>
+    (this.opacity = isNumber(opacity) ? Math.max(0, Math.min(1, opacity)) : this.opacity);
 }
 
 export default RushLayer;
